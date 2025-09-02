db516505e661199d4d17056084524156c4978689
Lucas Josino <contact@lucasjosino.com>
2025-09-01 21:07:32 -0300

    feat: add array file

---
+++ array.py
@@ -0,0 +1,10 @@
+array = ["Lucas", "Amanda", "Ana", "David", "Pedro"]
+
+print("Primeiro elemento:", array[0])
+print("Terceiro elemento:", array[2])
+print("Último elemento:", array[-1])
+
+if "Carla" in array:
+    print("Carla está na lista!")
+else:
+    print("Carla não foi encontrada na lista.")
